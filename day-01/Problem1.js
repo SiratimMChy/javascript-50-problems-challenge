b9f79ca9b1a816bc38075ed845606fc1647787dd
Tuha chy <siratimmustakimchowdhury123@gmail.com>
2026-08-18 13:33:42 +0600

    Day 1

---
+++ day-01/Problem1.js
@@ -0,0 +1,13 @@
+/*Problem 1: Check whether a number is positive, negative, or zero.*/
+
+let number = -1;
+
+if (number > 0) {
+    console.log("Number is positive");
+}
+else if (number < 0) {
+    console.log("Number is negative");
+}
+else {
+    console.log("Number is zeo or neutral");
+}
