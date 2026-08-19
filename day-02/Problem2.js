d568d5cf478064e09575f91b98a33c29f4b256fa
Tuha chy <siratimmustakimchowdhury123@gmail.com>
2026-08-19 20:17:43 +0600

    Update solution of problem 2

--- day-02/Problem2.js
+++ day-02/Problem2.js
@@ -1,14 +1,14 @@
-let nums = [1,2,3,4,5];
+let nums = [1, 2, 3, 4, 5];
 let largest = nums[0];
 let secondlargest = nums[0];
 for (let i = 0; i < nums.length; i++) {
-   if(nums[i]>=largest){
-    secondlargest = largest;
-    largest = nums[i];
-   }
-   if( secondlargest >  nums[i]){
-    secondlargest =  nums[i];
-   }
+    if (nums[i] >= largest) {
+        secondlargest = largest;
+        largest = nums[i];
+    }
+    if ((nums[i] > secondlargest) && (nums[i] < largest)) {
+        secondlargest = nums[i];
+    }
 }
 
 console.log(secondlargest);
