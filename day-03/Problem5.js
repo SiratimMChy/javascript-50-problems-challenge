fc52f669b5823a2ca67086b9839228a1b57ac2d0
Tuha chy <siratimmustakimchowdhury123@gmail.com>
2026-08-20 19:26:29 +0600

    Add solutions of Day 3 problem 5

--- day-03/Problem5.js
+++ day-03/Problem5.js
@@ -1,1 +1,16 @@
 /* Problem 21: Calculate the sum of all numeric values in an object.*/
+let obj = {
+    a: 10,
+    b: 20,
+    c: "Hello",
+    d: 30
+};
+let sum = 0;
+for (const value in obj) {
+    const element = obj[value];
+    if (typeof element === "number") {
+        sum+=obj[value];
+    }
+}
+
+console.log(sum);
